63c232843c06d4278a8926f0e1c2d1f8c1c97495
Zan Preston <zan.preston@banno.com>
2024-01-03 14:31:06 -0600

    Change colorscheme to cyberdream

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,26 @@
 return {
+  -- {
+  --   "catppuccin/nvim",
+  --   lazy = true,
+  --   name = "catppuccin",
+  -- },
   {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+      })
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      -- colorscheme = "catppuccin",
+      colorscheme = "cyberdream",
     },
   },
 }
