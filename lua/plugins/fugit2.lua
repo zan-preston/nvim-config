89e707442447f911d108f46659bfba6092b90bb3
Zan Preston <zan.preston@banno.com>
2024-04-23 16:19:47 -0500

    Maybe use fugit2 one day, not now

---
+++ lua/plugins/fugit2.lua
@@ -0,0 +1,34 @@
+return {
+  -- {
+  --   "SuperBo/fugit2.nvim",
+  --   opts = {
+  --     width = 70,
+  --   },
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-tree/nvim-web-devicons",
+  --     "nvim-lua/plenary.nvim",
+  --     {
+  --       "chrisgrieser/nvim-tinygit", -- optional: for Github PR view
+  --       dependencies = { "stevearc/dressing.nvim" },
+  --     },
+  --   },
+  --   cmd = { "Fugit2", "Fugit2Graph" },
+  --   keys = {
+  --     { "<leader>gF", "<cmd>Fugit2<cr>", desc = "Fugit2" },
+  --   },
+  -- },
+  -- {
+  --   -- optional: for diffview.nvim integration
+  --   "sindrets/diffview.nvim",
+  --   dependencies = { "nvim-tree/nvim-web-devicons" },
+  --   -- lazy, only load diffview by these commands
+  --   cmd = {
+  --     "DiffviewFileHistory",
+  --     "DiffviewOpen",
+  --     "DiffviewToggleFiles",
+  --     "DiffviewFocusFiles",
+  --     "DiffviewRefresh",
+  --   },
+  -- },
+}
