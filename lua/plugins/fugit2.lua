5698683dc6db788ac92fb892eb6b5208c48860c6
Zan Preston <zan.preston@banno.com>
2024-05-16 09:05:40 -0500

    add fugit2.nvim

--- lua/plugins/fugit2.lua
+++ lua/plugins/fugit2.lua
@@ -1,34 +1,35 @@
 return {
-  -- {
-  --   "SuperBo/fugit2.nvim",
-  --   opts = {
-  --     width = 70,
-  --   },
-  --   dependencies = {
-  --     "MunifTanjim/nui.nvim",
-  --     "nvim-tree/nvim-web-devicons",
-  --     "nvim-lua/plenary.nvim",
-  --     {
-  --       "chrisgrieser/nvim-tinygit", -- optional: for Github PR view
-  --       dependencies = { "stevearc/dressing.nvim" },
-  --     },
-  --   },
-  --   cmd = { "Fugit2", "Fugit2Graph" },
-  --   keys = {
-  --     { "<leader>gF", "<cmd>Fugit2<cr>", desc = "Fugit2" },
-  --   },
-  -- },
-  -- {
-  --   -- optional: for diffview.nvim integration
-  --   "sindrets/diffview.nvim",
-  --   dependencies = { "nvim-tree/nvim-web-devicons" },
-  --   -- lazy, only load diffview by these commands
-  --   cmd = {
-  --     "DiffviewFileHistory",
-  --     "DiffviewOpen",
-  --     "DiffviewToggleFiles",
-  --     "DiffviewFocusFiles",
-  --     "DiffviewRefresh",
-  --   },
-  -- },
+  {
+    "SuperBo/fugit2.nvim",
+    opts = {
+      libgit2_path = "/opt/homebrew/lib/libgit2.dylib",
+      width = 70,
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "nvim-lua/plenary.nvim",
+      {
+        "chrisgrieser/nvim-tinygit", -- optional: for Github PR view
+        dependencies = { "stevearc/dressing.nvim" },
+      },
+    },
+    cmd = { "Fugit2", "Fugit2Graph", "Fugit2Diff" },
+    keys = {
+      { "<leader>gF", "<cmd>Fugit2<cr>", desc = "Fugit2" },
+    },
+  },
+  {
+    -- optional: for diffview.nvim integration
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- lazy, only load diffview by these commands
+    cmd = {
+      "DiffviewFileHistory",
+      "DiffviewOpen",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+      "DiffviewRefresh",
+    },
+  },
 }
